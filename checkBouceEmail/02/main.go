c18eea7de0c6940e6d4187aec693c08a206b82be
pierangelo <pierangelo1982@gmail.com>
2019-04-03 11:50:11 +0200

    arse csv

---
+++ checkBouceEmail/02/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+type Subscriber struct {
+	Name  string
+	Email string
+}
+
+func main() {
+	// Open the file
+	csvfile, _ := os.Open("mycsv.csv")
+	// parse the file
+	reader := csv.NewReader(csvfile)
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(record[1])
+	}
+
+}
